b250257c32b8f7b251d9d328e9925c4c99d84028
dterc001 <33464931+dterc001@users.noreply.github.com>
2017-11-07 14:37:30 -0500

    Add files via upload

---
+++ GameFactory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+
+
+namespace Game
+{
+    public class GameFactory
+    {
+        List<Games> gameList;
+        StreamWriter sw;
+        XmlSerializer serial;
+        const String FILEPATH = @"..\..\Games.xml";
+        
+
+      public void CreateGameList()
+        {
+            gameList = new List<Games>();
+            Games G = new Games("Panthers", 20, "Dolphins", 15);
+
+            gameList.Add(G);
+
+            G = new Games("Alligators", 9, "Bears", 7);
+
+            gameList.Add(G);
+            G = new Games("Kangaroo", 15, "Iguana", 12);
+
+            gameList.Add(G);
+            G = new Games("Frogs", 99, "Turtles", 99);
+
+            gameList.Add(G);
+            G = new Games("Wolves", 29, "Cheetah", 29);
+
+            gameList.Add(G);
+            G = new Games("Cubs", 2, "Bobcats", 6);
+
+            gameList.Add(G);
+
+            serial = new XmlSerializer(gameList.GetType());
+            sw = new StreamWriter(FILEPATH);
+            serial.Serialize(sw, gameList);
+
+            
+            sw.Close();
+
+        }
+
+       /* public List<Games> DeserializeGameList()
+        {
+
+            gameList = new List<Games>();
+            StreamReader sr = new StreamReader(FILEPATH);
+            serial = new XmlSerializer(gameList.GetType());
+            gameList = (List<Games>)serial.Deserialize(sr);
+
+            
+            sr.Close();
+            return gameList;
+            
+
+           
+        
+
+        }*/
+
+    }
+}
---
+++ Games.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Games
+    {
+        public string Team1 { get; set; }
+
+        public int Team1Score { get; set; }
+        public string Team2 { get; set; }
+
+        public int Team2Score { get; set; }
+
+        public Games() { }
+        public Games (string Team1, int Team1Score, string Team2, int Team2Score)
+
+        {
+            this.Team1 = Team1;
+            this.Team1Score = Team1Score;
+            this.Team2 = Team2;
+            this.Team2Score = Team2Score;
+        }
+
+        public override string ToString()
+        {
+
+            string score1 = "(" + Team1Score + ")  -";
+            string score2 = "(" + Team2Score + ")";
+
+
+            return String.Format("{0} {1} {2} {3}", Team1, score1, Team2, score2);
+        }
+
+      
+
+    }
+
+   
+}
